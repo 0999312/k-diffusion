b979d36951f99452a128b7f1cc22d9e66dc593a5
unknown <617510630@qq.com>
2023-06-22 04:48:49 +0800

    DPM-2M Sharp V1 FIX

--- k_diffusion/sampling.py
+++ k_diffusion/sampling.py
@@ -644,7 +644,12 @@ def sample_dpmpp_2m_sharp_v1(model, x, sigmas, extra_args=None, callback=None, d
     extra_args = {} if extra_args is None else extra_args
     s_in = x.new_ones([x.shape[0]])
     sigma_fn = lambda t: t.neg().exp()
-    t_fn = lambda sigma: sigma.log().neg()
+    # MacOS fix
+    if torch.backends.mps.is_available() and torch.backends.mps.is_built():
+        t_fn = lambda sigma: sigma.detach().clone().log().neg()
+    else:
+        t_fn = lambda sigma: sigma.log().neg()
+
     old_denoised = None
 
     for i in trange(len(sigmas) - 1, disable=disable):
@@ -653,13 +658,18 @@ def sample_dpmpp_2m_sharp_v1(model, x, sigmas, extra_args=None, callback=None, d
             callback({'x': x, 'i': i, 'sigma': sigmas[i], 'sigma_hat': sigmas[i], 'denoised': denoised})
         t, t_next = t_fn(sigmas[i]), t_fn(sigmas[i + 1])
         h = t_next - t
+        t_min = min(sigma_fn(t_next), sigma_fn(t))
+        t_max = max(sigma_fn(t_next), sigma_fn(t))
         if old_denoised is None or sigmas[i + 1] == 0:
-            x = (sigma_fn(t_next) / sigma_fn(t)) * x - (-h).expm1() * denoised
+            x = (t_min / t_max) * x - (-h).expm1() * denoised
         else:
             h_last = t - t_fn(sigmas[i - 1])
-            r = h_last / h
+            h_min = min(h_last, h)
+            h_max = max(h_last, h)
+            r = h_max / h_min
+            h_d = (h_max + h_min) / 2
             denoised_d = (1 + 1 / (2 * r)) * denoised - (1 / (2 * r)) * old_denoised
-            x = (sigma_fn(t_next) / sigma_fn(t)) * x - (-h).expm1() * denoised_d
+            x = (t_min / t_max) * x - (-h_d).expm1() * denoised_d
         sigma_progress = i / len(sigmas)
         adjustment_factor = 1 + (0.15 * (sigma_progress * sigma_progress))
         old_denoised = denoised * adjustment_factor
